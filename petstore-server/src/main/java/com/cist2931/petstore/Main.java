3dab8779c236904d5ec29ca60b6e9c3ce6b2b007
Banks Troutman <btrout.dhrs@gmail.com>
2020-09-03 17:05:10 -0400

    Update database connection string, add customer test print

--- petstore-server/src/main/java/com/cist2931/petstore/Main.java
+++ petstore-server/src/main/java/com/cist2931/petstore/Main.java
@@ -3,6 +3,10 @@ package com.cist2931.petstore;
 import com.cist2931.petstore.database.DatabaseManager;
 import com.cist2931.petstore.logging.LogLevel;
 import com.cist2931.petstore.logging.Logger;
+import com.cist2931.petstore.objects.Customer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main {
     private static final Logger logger = new Logger(Main.class);
@@ -16,14 +20,25 @@ public class Main {
             logger.error("No database driver, exiting program.");
             return;
         } else {
-            if (!dbManager.initConnection("172.16.29.1", "petStoreWeb", "902251430D2CDB269FB6CF11F66704E3", "petstore-testing")) {
+            if (!dbManager.initConnection("68.117.215.47:8291", "petStoreWeb", "902251430D2CDB269FB6CF11F66704E3", "petstoreOne")) {
                 logger.error("No database connection, exiting program.");
                 return;
             }
         }
         logger.info("SQL database init complete.");
 
-        dbManager.runQuery("SELECT * FROM testTable");
+        Customer bankst = null;
+        try {
+            bankst = Customer.GetCustomerByID(dbManager.getDbConnection(), 0);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        if (bankst != null) {
+            logger.info("Got bankst!");
+            logger.debug(bankst.toString());
+        }
+//        ResultSet testTableData = dbManager.runQuery("INSERT INTO Customer (Password, FirstName, LastName, Street, City, State, Zipcode, Email) VALUES ('testpassword', 'John', 'Doe', '123 Main St', 'Townsville', 123456, 'Georgia')");
     }
 
 }
