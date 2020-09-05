f05980bcd4d91f1e9ae011a0d9ddb541ec61109f
Patrick <patrick300678@gmail.com>
2020-09-05 17:37:26 -0400

    Added insert update and delete to Merchandise.

--- petstore-server/src/main/java/com/cist2931/petstore/objects/Merchandise.java
+++ petstore-server/src/main/java/com/cist2931/petstore/objects/Merchandise.java
@@ -30,7 +30,7 @@ public class Merchandise {
         price = rs.getDouble("Price");
         category = rs.getString("Category");
         description = rs.getString("Description");
-        quantity = rs.getInt("Quantity on Hand");
+        quantity = rs.getInt("Quantity");
     }
 
     public static Merchandise GetMerchByID(Connection dbConnection, int id) throws SQLException {
@@ -42,6 +42,40 @@ public class Merchandise {
         if (resultSet.next()) {
             return new Merchandise(resultSet);
         } else return null;
+    }
+
+    public static Boolean InsertMerch(Connection dbConnection, int merchID, String merchName, double price, String category, String description, int quantity) throws SQLException {
+        final String insertQuery = "INSERT INTO Merchandise(MerchID, MerchName, Price, Category, Description, Quantity) values (?, ?, ?, ?, ?, ?)";
+        PreparedStatement statement = dbConnection.prepareStatement(insertQuery);
+        statement.setInt(1, merchID);
+        statement.setString(2, merchName);
+        statement.setDouble(3, price);
+        statement.setString(4, category);
+        statement.setString(5, description);
+        statement.setInt(6, quantity);
+
+        return statement.executeUpdate() == 1;
+    }
+
+    public static Boolean UpdateMerch(Connection dbConnection, int merchID, String merchName, double price, String category, String description, int quantity) throws SQLException {
+        final String updateQuery = "UPDATE Merchandise set MerchName = ?, Price = ?, Category = ?, Description = ?, Quantity = ? WHERE MerchID = ?";
+        PreparedStatement statement = dbConnection.prepareStatement(updateQuery);
+        statement.setString(1, merchName);
+        statement.setDouble(2, price);
+        statement.setString(3, category);
+        statement.setString(4, description);
+        statement.setInt(5, quantity);
+        statement.setInt(6, merchID);
+
+        return statement.executeUpdate() == 1;
+    }
+
+    public static Boolean DeleteMerch(Connection dbConnection, int merchID) throws SQLException {
+        final String deleteQuery = "DELETE FROM Merchandise WHERE MerchID = ?";
+        PreparedStatement statement = dbConnection.prepareStatement(deleteQuery);
+        statement.setInt(1, merchID);
+
+        return statement.executeUpdate() == 1;
     }
 
     @Override
