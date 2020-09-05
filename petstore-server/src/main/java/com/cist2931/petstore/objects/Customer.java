4c17ceb007f15d3dbbd373de64be4c3d992bdc88
Patrick <patrick300678@gmail.com>
2020-09-05 09:28:28 -0400

    Added password variable, changed variables to private, added getters and setters.

--- petstore-server/src/main/java/com/cist2931/petstore/objects/Customer.java
+++ petstore-server/src/main/java/com/cist2931/petstore/objects/Customer.java
@@ -7,18 +7,33 @@ import java.sql.SQLException;
 
 public class Customer {
 
-    public int customerID;
-    public String firstName;
-    public String lastName;
-    public String street;
-    public String city;
-    public int zipcode;
-    public String phoneNumber;
-    public String email;
+    private int customerID;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private int zipcode;
+    private String phoneNumber;
+    private String email;
+
+    public Customer() {
+        //default constructor
+        customerID = 0;
+        password = "";
+        firstName = "";
+        lastName = "";
+        street = "";
+        city = "";
+        zipcode = 0;
+        phoneNumber = "";
+        email = "";
+    }
 
     public Customer(ResultSet rs) throws SQLException {
         // initialize from result set
         customerID = rs.getInt("CustID");
+        password = rs.getString("Password");
         firstName = rs.getString("FirstName");
         lastName = rs.getString("LastName");
         street = rs.getString("Street");
@@ -27,22 +42,6 @@ public class Customer {
         phoneNumber = rs.getString("PhoneNum");
         email = rs.getString("Email");
     }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerID=" + customerID +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", street='" + street + '\'' +
-                ", city='" + city + '\'' +
-                ", zipcode=" + zipcode +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-
 
     public static Customer GetCustomerByID(Connection dbConnection, int id) throws SQLException {
         final String selectQuery = "SELECT * FROM Customer WHERE CustID = ?";
@@ -53,5 +52,92 @@ public class Customer {
         if (resultSet.next()) {
             return new Customer(resultSet);
         } else return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerID=" + customerID +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", zipcode=" + zipcode +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
