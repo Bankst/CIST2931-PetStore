fc9c723f40ea46a85709b770f74ce5e30e5049de
Banks Troutman <btrout.dhrs@gmail.com>
2020-09-15 17:20:15 -0400

    Convert DatabaseManager to Singleton class

--- petstore-server/src/main/java/com/cist2931/petstore/database/DatabaseManager.java
+++ petstore-server/src/main/java/com/cist2931/petstore/database/DatabaseManager.java
@@ -10,6 +10,14 @@ public class DatabaseManager {
 
     private Connection dbConnection;
 
+    private static final class DatabaseManagerSingleton {
+        public static final DatabaseManager SINGLETON = new DatabaseManager();
+    }
+
+    public static DatabaseManager getInstance() {
+        return DatabaseManagerSingleton.SINGLETON;
+    }
+
     public DatabaseManager() {}
 
     public boolean loadDriver() {
@@ -37,49 +45,7 @@ public class DatabaseManager {
         return true;
     }
 
-    public Connection getDbConnection() {
+    public Connection getConnection() {
         return dbConnection;
-    }
-
-    public ResultSet runQuery(String query) {
-        ResultSet resultSet = null;
-        try {
-            Statement statement = dbConnection.createStatement();
-            if (query.startsWith("SELECT")) {
-                if (statement.execute(query)) {
-                    resultSet = statement.getResultSet();
-                }
-            }
-            if (resultSet != null) {
-                dumpAllSelect(resultSet);
-            }
-
-        } catch (SQLException e) {
-            logger.error("Failed to run query!", e);
-            logger.error("SQLError: " + e.getSQLState());
-            logger.error("VendorError: " + e.getErrorCode());
-        }
-        return resultSet;
-    }
-
-
-    private void dumpAllSelect(ResultSet resultSet) throws SQLException {
-        ResultSetMetaData rsmd = resultSet.getMetaData();
-        int columnsNumber = rsmd.getColumnCount();
-
-        for (int i = 1; i <= columnsNumber; i++) {
-            System.out.print(rsmd.getColumnName(i) + "\t");
-        }
-        System.out.println();
-
-        while (resultSet.next()) {
-
-            for (int i = 1; i <= columnsNumber; i++) {
-                if (i > 1) System.out.print(",  ");
-                String columnValue = resultSet.getString(i);
-                System.out.print(columnValue);
-            }
-            System.out.println();
-        }
     }
 }
